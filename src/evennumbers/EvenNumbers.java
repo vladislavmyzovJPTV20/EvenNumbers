8d2a1c0e9058381592f4441921e52852ce7eaade
Vladislav Myzov <mozov.vlad@mail.ru>
2021-09-28 21:11:01 +0300

    Заполняем массив 20 случайными чётными числами и находим среднее арифметическое чисел без учёта минимального и максимального значений

--- src/evennumbers/EvenNumbers.java
+++ src/evennumbers/EvenNumbers.java
@@ -18,11 +18,12 @@ public class EvenNumbers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Создаем массив из 20 элементов
         int[]even_numbers;
         even_numbers = new int[20];
         int i = 0;
-        
         Random random_numbers = new Random();
+        // Запускаем цикл while и заполняем его до тех пор, пока переменная i < 20 и заполняем массив случайными чётными числами
         while(i < 20) {
             int num = random_numbers.nextInt(100);
             if(num % 2 == 0) {
@@ -31,8 +32,13 @@ public class EvenNumbers {
                 
             }
         }
+        // Выводим весь массив на экран
         System.out.println("Все элементы массива = " + Arrays.toString(even_numbers));
         
+        // Далее находим максимальное значение при помощи цикла for, в котором с помощью условного оператор
+        // if мы проверяем, является ли каждое последующее число массива больше предыдущего
+        // В итоге переменной maximum мы присваеваем элемент максимального числа данного массива
+        // И выводим на экран максимальный элемент массива
         int maximum = 0;
         for(int j = 0; j < even_numbers.length; j++) {
             if(even_numbers[j] > maximum) {
@@ -41,6 +47,7 @@ public class EvenNumbers {
         }
         System.out.println("Максимальный элемент массива: " + maximum);
         
+        // Точно так же находим и минимальный элемент массива и выводим его на экран
         int minimum = 30;
         for(int c = 0; c < even_numbers.length; c++) {
             if(even_numbers[c] < minimum) {
@@ -50,6 +57,7 @@ public class EvenNumbers {
         System.out.println("Минимальный элемент массива: " + minimum);
         System.out.println("");
         
+        // Далее мы находим сколько всего максимальных и минимальных чисел и выводим их количество на экран
         int max_numbers = 0;
         for(i=0;i<even_numbers.length;i++) {
             if(even_numbers[i] == maximum) {
@@ -66,10 +74,13 @@ public class EvenNumbers {
         }
         System.out.println("Всего минимальных чисел в массиве: " + min_numbers);
         
+        // Создаем новый массив, в котором будут числа, за исключением максимальных и минимальных
         int[] remainder_numbers = new int[even_numbers.length - (max_numbers + min_numbers)];
+        // Спортируем старый массив и копируем из старого в новый значения
         Arrays.sort(even_numbers);
         System.arraycopy(even_numbers, 0, remainder_numbers, 0, remainder_numbers.length);
         float average = 0;
+        // Запускаем цикл и суммируем все числа нового массива, далее по формуле выводим среднее арифметическое чисел массива
         for(i=0;i<remainder_numbers.length;i++) {
             average += remainder_numbers[i];
         }
